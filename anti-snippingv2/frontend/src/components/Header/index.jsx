844abed7ded2369f4efed7cd9c87b495403cca03
Evgsevg <babbyboss@mail.ru>
2023-03-31 01:06:54 +0300

    Add files via upload
    v2 add new start options in node js

---
+++ anti-snippingv2/frontend/src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css';
+function index() {
+  return (
+    <div>
+        <header>
+  <div class="inner">
+    <h1>Lorem ipsum dolor.</h1>
+    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, eligendi.</span>
+  </div>
+</header>
+    </div>
+  )
+}
+
+export default index
